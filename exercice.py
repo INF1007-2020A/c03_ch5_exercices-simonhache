6febb0c557083d40daea6030afc22bfee71d59d4
simonhache <simonhache17@gmail.com>
2020-10-20 21:30:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,6 +24,7 @@ def prime_integer_summation() -> int:
     for i in range(100):
         for n in range(len(prime_number_list)):
             if i%n != 0:
+                not_a_prime
                 break
                             
     
